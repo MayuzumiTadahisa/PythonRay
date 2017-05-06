504173288e1ac04c0e96dbb0ca51846859e708e4
MayuzumiTadahisa <mayutti@silk.plala.or.jp>
2017-05-06 21:47:57 +0900

    create
    Create new python project

---
+++ self_introduction2.py
@@ -0,0 +1,5 @@
+print('レイ')
+print('パイソン研究所')
+print(16)
+print(158.5)
+print(40.0)
---
+++ self_introduction3.py
@@ -0,0 +1,10 @@
+name = 'レイ'
+address = 'パイソン研究所'
+age = 16
+height = 158.5
+weight = 40.0
+
+print('あたしの名前は' + name + 'です')
+print('年は' + str(age))
+print(address + 'に住んでいます')
+print('身長は' + str(height) + 'cm、' + '体重は' + str(weight) + 'kgだよ')
